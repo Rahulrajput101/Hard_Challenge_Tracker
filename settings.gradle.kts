38d9f7e000964cd0c08136d0907eb8363e86220a
Rahulrajput101 <itsmerahul10534952@gmail.com>
2023-10-23 13:21:42 +0530

    Init: Setting Up the Modular Structure

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "75 Hard"
+include(":app")
+include(":core")
+include(":onboarding")
+include(":onboarding:onboarding_presentation")
+include(":onboarding:onboarding_domain")
+include(":login")
+include(":login:login_domain")
+include(":login:login_presentation")
+include(":login:login_data")
+include(":tracker")
+include(":tracker:tracker_data")
+include(":tracker:tracker_domain")
+include(":tracker:mylibrary")
