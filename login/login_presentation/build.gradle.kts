7bd6eaefecdf242e880eec9eb74a16f000fa1f2d
Rahulrajput101 <itsmerahul10534952@gmail.com>
2023-10-28 17:42:42 +0530

    Upd: login gradles

--- login/login_presentation/build.gradle.kts
+++ login/login_presentation/build.gradle.kts
@@ -1,48 +1,16 @@
-//@file:Suppress("DSL_SCOPE_VIOLATION")
-//plugins {
-//    alias(libs.plugins.android.application)
-//    alias(libs.plugins.android.kotlin)
-//
-//}
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    `android-library`
+    `kotlin-android`
 }
+
+apply(from = "$rootDir/compose-module.gradle")
 
 android {
     namespace = "com.ondevop.login_presentation"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
+    implementation(project(Modules.core))
+    implementation(project(Modules.loginDomain))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
